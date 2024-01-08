08459a5b5783009662a859cb3bfef15ae692c14e
fikisug <sugandafiki0@gmail.com>
2024-01-08 14:44:08 +0700

    Pesan commit Anda di sini

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index(Request $request) {
+
+        $tahun = $request["tahun"];
+        $makanan = array();
+        $minuman = array();
+        $arrTotalMenuPerTahun = array();
+        $arrTotalPerBulan = array();
+        $arrTotalMenuPerBulan = array();
+        $subTotal = 0;
+        
+        $bulan = [
+            [
+                'bulan' => '01',
+                'nama'  => 'Jan'
+            ],
+            [
+                'bulan' => '02',
+                'nama'  => 'Feb'
+            ],
+            [
+                'bulan' => '03',
+                'nama'  => 'Mar'
+            ],
+            [
+                'bulan' => '04',
+                'nama'  => 'Apr'
+            ],
+            [
+                'bulan' => '05',
+                'nama'  => 'Mei'
+            ],
+            [
+                'bulan' => '06',
+                'nama'  => 'Jun'
+            ],
+            [
+                'bulan' => '07',
+                'nama'  => 'Jul'
+            ],
+            [
+                'bulan' => '08',
+                'nama'  => 'Ags'
+            ],
+            [
+                'bulan' => '09',
+                'nama'  => 'Sep'
+            ],
+            [
+                'bulan' => '10',
+                'nama'  => 'Okt'
+            ],
+            [
+                'bulan' => '11',
+                'nama'  => 'Nov'
+            ],
+            [
+                'bulan' => '12',
+                'nama'  => 'Des'
+            ],
+        ];
+
+        if($tahun != "") {            
+            $menu = json_decode(file_get_contents("http://tes-web.landa.id/intermediate/menu"), true);
+            $transaksi = json_decode(file_get_contents("http://tes-web.landa.id/intermediate/transaksi?tahun=".$request["tahun"]), true);    
+            
+            foreach($menu as $menu) {
+                if($menu["kategori"] == 'makanan') {
+                    $makanan[]["makanan"] = $menu["menu"];
+                }
+                
+                if($menu["kategori"] == 'minuman') {
+                    $minuman[]["minuman"] = $menu["menu"];
+                }
+                
+                
+                foreach($bulan as $b) {
+
+                    $totalMenuPerBulan = 0;
+                    $totalPerMenu = 0;
+                    $totalPerBulan = 0;
+
+                    foreach($transaksi as $t) {
+                        $timestamps = strtotime($t['tanggal']);
+
+                        $formatbulan = date("m", $timestamps);
+                        
+                        if($formatbulan == $b['bulan']) {
+                            $totalPerBulan = $totalPerBulan + $t["total"];
+                        }
+
+                        if($menu["menu"] == $t["menu"]) {
+                            $totalPerMenu = $totalPerMenu + $t["total"];
+                        }
+
+                        if($formatbulan == $b['bulan'] && $menu['menu'] == $t["menu"]) {
+                            $totalMenuPerBulan = $totalMenuPerBulan + $t["total"];
+                        }
+                        
+                        $arrTotalPerBulan[$b["bulan"]] = $totalPerBulan;
+                    }
+                    $arrTotalMenuPerBulan[$menu["menu"]][]["total"] = $totalMenuPerBulan;
+                }                
+                $arrTotalMenuPerTahun[$menu['menu']]["subtotal"] = $totalPerMenu;
+            }
+            foreach($arrTotalPerBulan as $totalPerBulan) {
+                
+                $subTotal += $totalPerBulan;
+            }
+        }
+        // dd($arrTotalMenuPerBulan);
+        return view('welcome', [
+            "namaBulan" => $bulan,
+            "tahun" => $tahun,
+            "makanan" => $makanan,
+            "minuman" => $minuman,
+            "totalMenuPerBulan" => $arrTotalMenuPerBulan,
+            "totalPerBulan" => $arrTotalPerBulan,
+            "totalMenuPerTahun" => $arrTotalMenuPerTahun,
+            "subTotal" => $subTotal,
+        ]);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style>
+        td,
+        th {
+            font-size: 11px;
+        }
+    </style>
+
+
+    <title>TES - Venturo Camp Tahap 2</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="card" style="margin: 2rem 0rem;">
+            <div class="card-header">
+                Venturo - Laporan penjualan tahunan per menu
+            </div>
+            <div class="card-body">
+                <form action="/" method="get">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="form-group">
+                                <select id="my-select" class="form-control" name="tahun">
+
+                                    @if (!isset($_GET['tahun']))
+                                        <option value="" selected>Pilih Tahun</option>
+                                        <option value="2021">2021</option>
+                                        <option value="2022">2022</option>
+                                    @elseif(isset($_GET['tahun']) == "2021")
+                                        <option value="">Pilih Tahun</option>
+                                        <option value="2021" selected>2021</option>
+                                        <option value="2022">2022</option>
+                                    @elseif(isset($_GET['tahun']) == "2022")
+                                        <option value="">Pilih Tahun</option>
+                                        <option value="2021">2021</option>
+                                        <option value="2022" selected>2022</option>
+                                    @elseif(isset($_GET['tahun']) == "")
+                                        <option value="" selected>Pilih Tahun</option>
+                                        <option value="2021">2021</option>
+                                        <option value="2022">2022</option>
+                                    @endif
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary">
+                                Tampilkan
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                <hr>
+                <div class="table-responsive">
+                    <table class="table table-hover table-bordered" style="margin: 0;">
+                        <thead>
+                            <tr class="table-dark">
+                                <th rowspan="2" style="text-align:center;vertical-align: middle;width: 250px;">Menu</th>
+                                    @if (isset($_GET['tahun']) != "")
+                                        <th colspan="12" style="text-align: center;">Periode Pada  {{ ($_GET['tahun']) ? $tahun : '' }}
+                                    @else
+                                        <th colspan="12" style="text-align: center;">Periode Pada 
+                                    @endif
+                                </th>
+                                <th rowspan="2" style="text-align:center;vertical-align: middle;width:75px">Total</th>
+                            </tr>
+                            <tr class="table-dark">
+                                @foreach ($namaBulan as $val)
+                                    <th style="text-align: center;width: 75px;">{{ $val['nama'] }}</th> 
+                                @endforeach
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- jika data ditemukan atau terdapat tahun yang dipilih dan tahun tidak kosong -->
+                            @if (isset($_GET['tahun']) != "")
+                                <tr>
+                                    <td class="table-secondary" colspan="14"><b>Makanan</b></td>
+                                </tr>
+                                @foreach($makanan as $value)
+                                    <tr>
+                                        <td>{{ $value["makanan"] }}</td>
+                                        @foreach($totalMenuPerBulan[$value["makanan"]] as $menu)
+                                            <td style="text-align: right;">
+                                                {{$menu["total"]}}
+                                            </td>
+                                        @endforeach
+                                        <td style="text-align: right; font-weight: bold;">{{ number_format($totalMenuPerTahun[$value["makanan"]]["subtotal"], 0, ',' , ',') }}</td>
+                                    </tr>
+                                @endforeach
+                                <tr>
+                                    <td class="table-secondary" colspan="14"><b>Minuman</b></td>
+                                </tr>
+                                @foreach($minuman as $value)
+                                <tr>
+                                    <td>{{ $value["minuman"] }}</td>
+                                    @foreach($totalMenuPerBulan[$value["minuman"]] as $menu)
+                                        <td style="text-align: right;">
+                                            {{ number_format($menu["total"], 0, ',' , ',') }}
+                                        </td>
+                                    @endforeach
+                                    <td style="text-align: right; font-weight: bold;">{{ number_format($totalMenuPerTahun[$value["minuman"]]["subtotal"], 0, ',' , ',') }}</td>
+                                </tr>
+                                @endforeach
+                                <tr class="table-dark">
+                                    <td class="text-center">Total</td>
+                                    @foreach($totalPerBulan as $total)
+                                    <td style="text-align: right;">
+                                        {{ number_format($total, 0, ',' , ',') }}
+                                    </td>
+                                    @endforeach
+                                    <td style="text-align: right; font-weight: bold;">{{ number_format($subTotal, 0, ',' , ',') }}</td>
+                                </tr>
+                            @else
+                                <!-- jika tidak memilih tahun -->
+                                <tr>
+                                    <td colspan="14" class="text-center">Data tidak ditemukan.</td>
+                                </tr>
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
